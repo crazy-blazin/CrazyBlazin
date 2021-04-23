37eb260a156205cc5b0eae8862a0496f53f89e17
Martin Røvang <martinrovang@gmail.com>
2021-04-23 21:21:00 +0200

    Increased lootbox rewards

--- economy_bot/dev/cbm_dev.py
+++ economy_bot/dev/cbm_dev.py
@@ -95,7 +95,7 @@ class EventHandler:
 
 class LootBox:
     def __init__(self):
-        self.coins = np.random.randint(0, 1000)
+        self.coins = np.random.randint(0, 5000)
         self.boosters = np.random.randint(0, 5)
 
 
