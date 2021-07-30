3d40a0a078d2087721c0eb1f33ac6346ab348535
Martin Soria Røvang <martinrovang@gmail.com>
2021-07-30 14:57:02 +0200

    .

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ def check_version():
     with open('version.txt', 'r') as f:
         ver = float(f.read())
     if version != ver:
-        os._exit()
+        os._exit(0)
 
 def read_db():
     try:
